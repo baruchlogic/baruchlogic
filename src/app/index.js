674f65e1e2b3f5fccbdd4d90225b1e38d761482b
Jesse Rappaport <jessetherappaport@gmail.com>
2019-04-16 08:41:39 -0400

    Get pretter/eslint working with React

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const App = () => (
-  <div>Hello World!</div>
-);
+const App = () => <div>Hello World!</div>;
 
-render(<App />, window.document.getElementById('root'))
+render(<App />, window.document.getElementById('root'));
