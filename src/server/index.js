a263ea7ac8ceb878b30f21ad80c4e253a43f4b22
Jesse Rappaport <jessetherappaport@gmail.com>
2019-04-18 13:50:19 -0400

    Revert "Add body-parser"
    This reverts commit d354f79ae2c0a4fb826cdb49270450b315a82621.

--- src/server/index.js
+++ src/server/index.js
@@ -2,12 +2,10 @@ const express = require('express');
 const path = require('path');
 const { query } = require('./db');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'dist')));
