e35c27663ef2a6710aa911ea305c98fc029cc1a3
Jesse Rappaport <jessetherappaport@gmail.com>
2019-04-18 12:25:26 -0400

    Keep tinkering with API calls

--- src/app/VideosContainer.js
+++ src/app/VideosContainer.js
@@ -6,7 +6,9 @@ class VideosContainer extends Component {
     console.log('GET VIDEOS');
     // NOTE: The first instance of getting data ftom the API
     // TODO: Use the libraries/wrappers
-    const videos = await fetch('http://localhost:5000/api/videos').then(res => res.json());
+    const videos = await fetch('http://localhost:5000/api/videos').then(res =>
+      res.json()
+    );
     console.log('videos', videos.rows);
   }
 
