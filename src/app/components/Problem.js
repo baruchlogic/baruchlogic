f29122324a1bc075d5285076dcae75e054859df0
Jesse Rappaport <jessetherappaport@gmail.com>
2019-06-08 21:39:24 -0400

    Conditionalize problem types

--- src/app/components/Problem.js
+++ src/app/components/Problem.js
@@ -28,13 +28,62 @@ const TrueFalse = ({ problem, setProblemResponse, value }) => (
   </div>
 );
 
+const MultipleChoice = ({ problem, setProblemResponse, value }) => {
+  const choices = problem.choices;
+  return (
+    <div>
+      {Object.keys(choices).sort().map(key => (
+        <label key={key}>
+          <input
+            type="radio"
+            checked={value === key}
+            onChange={() => {
+              setProblemResponse(problem.id, key);
+            }}
+            value={true}
+          />
+          {choices[key]}
+        </label>
+      ))}
+    </div>
+  );
+};
+
+const TruthTable = ({ problem, setProblemResponse, value }) => {
+  const getAtomicVariables = proposition => {
+    const result = new Set();
+    for (const letter of proposition) {
+      if (/[a-z]/i.test(letter)) {
+        result.add(letter);
+      }
+    }
+    return Array.from(result).sort();
+  };
+  const atomicVariables = getAtomicVariables(problem.prompt);
+  const nRows = 2 ** atomicVariables.length;
+};
+
 const Problem = ({ problem, setProblemResponse, value }) => {
+  let ProblemType;
+  switch (problem.type) {
+    case 'true_false':
+      ProblemType = TrueFalse;
+      break;
+    case 'multiple_choice':
+      ProblemType = MultipleChoice;
+      break;
+    case 'truth_table':
+      ProblemType = TruthTable;
+      break;
+    default:
+      break;
+  }
   return (
     <StyledCard key={problem.id}>
       <div>({problem.problem_index})</div>
       <div>
         <p>{problem.prompt}</p>
-        <TrueFalse
+        <ProblemType
           problem={problem}
           setProblemResponse={setProblemResponse}
           value={value}
