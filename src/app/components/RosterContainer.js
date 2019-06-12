98a705c1c1a0724819f5cf77fbd5fdf3cee0639e
Jesse Rappaport <jessetherappaport@gmail.com>
2019-06-11 22:46:51 -0400

    Weird linting stuff

--- src/app/components/RosterContainer.js
+++ src/app/components/RosterContainer.js
@@ -51,9 +51,7 @@ const RosterContainer = () => {
           >
             {instructorSections.map(section => (
               <option
-                key={`${section.section_number}-${section.term}-${
-                  section.year
-                }`}
+                key={`${section.section_number}-${section.term}-${section.year}`}
                 value={section.section_number}
                 selected={
                   currentSection &&
