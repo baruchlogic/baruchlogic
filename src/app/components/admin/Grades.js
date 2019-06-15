eb0e51e5143bde4cd44bd8041cf887b42845b7d1
Jesse Rappaport <jessetherappaport@gmail.com>
2019-06-15 16:34:57 -0400

    Extract custom hook for useInstructorSection

---
+++ src/app/components/admin/Grades.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import styled from 'styled-components';
+import { Elevation } from '@blueprintjs/core';
+import StyledCard from 'app-styled/StyledCard';
+
+const Grades = () => {
+  return (
+    <StyledCard elevation={Elevation.TWO}>
+      <div>Select a course section:</div>
+    </StyledCard>
+  );
+};
+
+export default Grades;
