b8496817bbe1751c07b75cf8943ec569ed195c0e
Jesse Rappaport <jessetherappaport@gmail.com>
2019-06-30 18:12:38 -0400

    Add Associativity

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,9 @@ const TEXTBOOK_URL = 'https://www.github.com/baruchlogic/textbook';
 
 const DEDUCTION_RULES = {
   ADDITION: 'Addition',
+  ASSOCIATIVITY: 'Associativity',
   CONJUNCTION: 'Conjunction',
+  DOUBLE_NEGATION: 'Double Negation',
   MODUS_PONENS: 'Modus Ponens',
   MODUS_TOLLENS: 'Modus Tollens',
   PREMISE: 'Premise',
