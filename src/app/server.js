0a5d95e3f468464f2b2a80520b5f57006d758088
Jesse Rappaport <jessetherappaport@gmail.com>
2019-07-13 12:54:26 -0400

    Set up frontend server - works with BE

--- src/app/server.js
+++ src/app/server.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
 
@@ -6,6 +7,8 @@ app.listen(port, () => {
   console.log('Listening on port ' + port);
 });
 
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
