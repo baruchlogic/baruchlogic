b088c22719fc838cb1ea9d0c0b35001306df8940
Jesse Rappaport <jessetherappaport@gmail.com>
2019-08-25 18:37:04 -0400

    Fix sidebar height

--- src/app/styled-components/StyledSidebar.js
+++ src/app/styled-components/StyledSidebar.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const StyledSidebar = styled.div`
   box-shadow: 1px 1px 2px 1px #2d0f4c;
   color: #2d0f4c;
-  height: 1000px;
+  min-height: 1000px;
   min-width: 300px;
   text-align: center;
   width: 300px;
