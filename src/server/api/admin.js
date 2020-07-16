7fd4b6e074ec3493e856bf87eaee8be103122c91
Jesse Rappaport <jessetherappaport@gmail.com>
2020-07-15 20:26:19 -0400

    WIP

--- src/server/api/admin.js
+++ src/server/api/admin.js
@@ -8,18 +8,13 @@ const { createNewUser } = require('../db/data-access-layer/user');
 const configAdminRoutes = app => {
   // Creates a new section and returns the section number and student keys
   app.post('/api/section', async (req, res) => {
-    console.log('/api/section');
-    console.log('BODY', req.body);
-    console.log('user', req.user);
     const { sectionNumber, nStudents, term, year } = req.body;
     const sectionExists = await checkIfSectionExists(req.body);
-    console.log('sectionExists', sectionExists);
     if (sectionExists) {
       res.sendStatus(400);
       return;
     }
     const { id: instructorId } = req.user;
-    console.log('INSTRUCTORID', instructorId, typeof instructorId);
     await createNewSectionWithInstructor(
       { sectionNumber, term, year },
       instructorId
@@ -27,7 +22,6 @@ const configAdminRoutes = app => {
     const studentKeys = [];
     for (let i = 0; i < Number(nStudents); i++) {
       const newKey = await createNewUser();
-      console.log('NEW KEY', newKey);
       studentKeys.push(newKey);
     }
     await addStudentsToSection({ sectionNumber, term, year }, studentKeys);
