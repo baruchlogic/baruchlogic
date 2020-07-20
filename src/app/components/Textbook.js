261142de830250c34d092b551eb016690d4f77fa
Jesse Rappaport <jessetherappaport@gmail.com>
2020-07-20 17:03:45 -0400

    Remove <br>s

--- src/app/components/Textbook.js
+++ src/app/components/Textbook.js
@@ -14,21 +14,24 @@ const StyledCard = styled(Card)`
 
 import { ADMIN_EMAIL, TEXTBOOK_URL } from 'constants';
 
-const Textbook = (text = '') => (
-  <StyledCard elevation={Elevation.THREE}>
-    <p>
-      The textbook for this course can be found&nbsp;
-      <a href={TEXTBOOK_URL}>here</a>.
-      <br />
-    </p>
-    <p>
-      If you wish to contribute to the text, or if you notice any mistakes or
-      errors, feel free to file an Issue or Pull Request on GitHub or, if you
-      don&apost; have a GitHub account, you may email
-      <a href={`mailto:${ADMIN_EMAIL}`}>{`${ADMIN_EMAIL}`}</a>.
-    </p>
-    <Markdown source={text.text} />
-  </StyledCard>
-);
+const Textbook = (text = { text: '' }) => {
+  const cleanedText = text.text.replace(/<br>/g, '\n');
+  return (
+    <StyledCard elevation={Elevation.THREE}>
+      <p>
+        The textbook for this course can be found&nbsp;
+        <a href={TEXTBOOK_URL}>here</a>.
+        <br />
+      </p>
+      <p>
+        If you wish to contribute to the text, or if you notice any mistakes or
+        errors, feel free to file an Issue or Pull Request on GitHub or, if you
+        don&apost; have a GitHub account, you may email
+        <a href={`mailto:${ADMIN_EMAIL}`}>{`${ADMIN_EMAIL}`}</a>.
+      </p>
+      <Markdown source={cleanedText} />
+    </StyledCard>
+  );
+};
 
 export default Textbook;
